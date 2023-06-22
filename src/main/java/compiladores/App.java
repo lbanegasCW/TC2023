30ec65b4d44f38cbd42a200ceea49fd6a42f4d34
Luis Banegas <lubanegas@ubp.edu.ar>
2023-06-22 14:19:19 -0300

    Revert "Add tabla de simbolos e informe de errores"
    This reverts commit 8e6c19648883062519d70574bcfefeb25f8c6213.

--- src/main/java/compiladores/App.java
+++ src/main/java/compiladores/App.java
@@ -13,33 +13,32 @@ public class App {
         CharStream input = CharStreams.fromFileName("input/entrada.txt");
 
         // create a lexer that feeds off of input CharStream
-        CompilerLexer lexer = new CompilerLexer(input);
+        compiladoresLexer lexer = new compiladoresLexer(input);
         
         // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         
         // create a parser that feeds off the tokens buffer
-        CompilerParser parser = new CompilerParser(tokens);
+        compiladoresParser parser = new compiladoresParser(tokens);
                 
         // create Listener
-        MyCompilerListener listener = new MyCompilerListener();
+        // ExpRegBaseListener escucha = new Escucha();
 
         // Conecto el objeto con Listeners al parser
-        parser.addParseListener(listener);
+        // parser.addParseListener(escucha);
 
         // Solicito al parser que comience indicando una regla gramatical
         // En este caso la regla es el simbolo inicial
-        System.out.println("");
-        ParseTree tree = parser.programa();
-
+        parser.s();
+        // ParseTree tree =  parser.s();
         // Conectamos el visitor
         // Caminante visitor = new Caminante();
         // visitor.visit(tree);
         // System.out.println(visitor);
         // System.out.println(visitor.getErrorNodes());
-        
         // Imprime el arbol obtenido
-        System.out.println(tree.toStringTree(parser));
+        // System.out.println(tree.toStringTree(parser));
+        // System.out.println(escucha);
         
     }
 }
--- src/main/python/compiladores.g4
+++ src/main/python/compiladores.g4
@@ -1,19 +1,15 @@
 grammar compiladores;
 
-@header {
-package compiladores;
-}
-
 fragment LETRA : [A-Za-z] ;
 fragment DIGITO : [0-9] ;
 
-PALABRA : (LETRA | '_')(LETRA | DIGITO | '_')+ ;
 NUMERO : DIGITO+ ;
-WS : [ \t\n\r] ;
 OTRO : . ;
 
-s : PALABRA { System.out.println("PALABRA ->" + $PALABRA.getText() + "<--"); }   s
-  | NUMERO { System.out.println("NUMERO ->" + $NUMERO.getText() + "<--"); } s
-  | OTRO   { System.out.println("Otro ->" + $OTRO.getText() + "<--"); }     s
+ID : (LETRA | '_')(LETRA | DIGITO | '_')+ ;
+
+s : ID     {print("ID ->" + $ID.text + "<--") }         s
+  | NUMERO {print("NUMERO ->" + $NUMERO.text + "<--") } s
+  | OTRO   {print("Otro ->" + $OTRO.text + "<--") }     s
   | EOF
   ;
